0b8c1f08f7a99f3471deb4cde4c10ba62adf3bff
defaultuser0 <defaultuser0@DESKTOP-FDVUPSA>
2019-08-05 20:51:13 +0900

    test5

--- src/bitgroup/Test2.java
+++ src/bitgroup/Test2.java
@@ -3,6 +3,6 @@ package bitgroup;
 public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("test2");
-		System.out.println("test3");
+		System.out.println("test33");
 	}
 }
