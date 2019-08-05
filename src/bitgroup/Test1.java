04e9f2da14385bf4188d1a4b51189bd7ab009c71
defaultuser0 <defaultuser0@DESKTOP-FDVUPSA>
2019-08-05 20:25:19 +0900

    test

--- src/bitgroup/Test1.java
+++ src/bitgroup/Test1.java
@@ -4,7 +4,7 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		System.out.println("test1 start ! !");
-		System.out.println("test  start ! !");
+		System.out.println("test  start !! !");
 	}
 
 }
